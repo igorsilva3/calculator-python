5d1423aa75db12b953f8769b0cb99ff9225dc510
igorsilva3 <igorbr2018@gmail.com>
2020-08-20 18:12:29 -0300

    Functions for calculate, clear, setText

--- app/program.py
+++ app/program.py
@@ -6,28 +6,52 @@ class Calculator(QtWidgets.QMainWindow, Ui_MainWindow):
         super(Calculator, self).__init__()
         self.setupUi(self)
 
-        self.btn0.clicked.connect(lambda: self.calculation(self.btn0.text()))
-        self.btn1.clicked.connect(lambda: self.calculation(self.btn1.text()))
-        self.btn2.clicked.connect(lambda: self.calculation(self.btn2.text()))
-        self.btn3.clicked.connect(lambda: self.calculation(self.btn3.text()))
-        self.btn4.clicked.connect(lambda: self.calculation(self.btn4.text()))
-        self.btn5.clicked.connect(lambda: self.calculation(self.btn5.text()))
-        self.btn6.clicked.connect(lambda: self.calculation(self.btn6.text()))
-        self.btn7.clicked.connect(lambda: self.calculation(self.btn7.text()))
-        self.btn8.clicked.connect(lambda: self.calculation(self.btn8.text()))
-        self.btn9.clicked.connect(lambda: self.calculation(self.btn9.text()))
-        self.btnVirg.clicked.connect(lambda: self.calculation(self.btnVirg.text()))
-        self.btnMult.clicked.connect(lambda: self.calculation(self.btnMult.text()))
-        self.btnSub.clicked.connect(lambda: self.calculation(self.btnSub.text()))
-        self.btnAdic.clicked.connect(lambda: self.calculation(self.btnAdic.text()))
-        self.btnDiv.clicked.connect(lambda: self.calculation(self.btnDiv.text()))
-        self.btnClear.clicked.connect(lambda: self.clear(self.btnClear.text()))
+        self.numbers = ""
+
+        self.btn0.clicked.connect(lambda: self.set_text_display(self.btn0.text()))
+        self.btn1.clicked.connect(lambda: self.set_text_display(self.btn1.text()))
+        self.btn2.clicked.connect(lambda: self.set_text_display(self.btn2.text()))
+        self.btn3.clicked.connect(lambda: self.set_text_display(self.btn3.text()))
+        self.btn4.clicked.connect(lambda: self.set_text_display(self.btn4.text()))
+        self.btn5.clicked.connect(lambda: self.set_text_display(self.btn5.text()))
+        self.btn6.clicked.connect(lambda: self.set_text_display(self.btn6.text()))
+        self.btn7.clicked.connect(lambda: self.set_text_display(self.btn7.text()))
+        self.btn8.clicked.connect(lambda: self.set_text_display(self.btn8.text()))
+        self.btn9.clicked.connect(lambda: self.set_text_display(self.btn9.text()))
+        self.btnVirg.clicked.connect(lambda: self.set_text_display(self.btnVirg.text()))
+        self.btnMult.clicked.connect(lambda: self.set_text_display(self.btnMult.text()))
+        self.btnSub.clicked.connect(lambda: self.set_text_display(self.btnSub.text()))
+        self.btnAdic.clicked.connect(lambda: self.set_text_display(self.btnAdic.text()))
+        self.btnDiv.clicked.connect(lambda: self.set_text_display(self.btnDiv.text()))
+        self.btnParent1.clicked.connect(lambda: self.set_text_display(self.btnParent1.text()))
+        self.btnParent2.clicked.connect(lambda: self.set_text_display(self.btnParent2.text()))
+        self.btnClear.clicked.connect(lambda: self.clear())
+        self.btnResult.clicked.connect(lambda: self.calculation())
     
-    def calculation(self, arg):
-        if arg == ",":
-            self.display.display(".")
-            
-        self.display.display(arg)
+    def set_text_display(self, num):
+        self.numbers = self.numbers + num 
+        self.display.setText(self.numbers)
     
-    def clear(self, arg):
-        self.display.display("0")
+    def calculation(self):
+        """ Returns the calculation of the equation on the display """
+        try:
+            # Performs the calculation
+            result = str(eval(self.numbers)) 
+
+            # Reset the numbers
+            self.numbers = ""
+
+            # Clear the display
+            self.display.setText(self.numbers)
+
+            # Puts the result on the display
+            self.set_text_display(result)
+
+        except:
+            self.numbers = ""
+            self.display.setText(self.numbers)
+            self.set_text_display(" error ")
+        
+    def clear(self):
+        self.numbers = ""
+        self.display.setText("0")
